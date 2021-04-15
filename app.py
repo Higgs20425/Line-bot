9b71bf488b514e3c33084dfef09b4ce085873793
Higgs <jack20425@gmail.com>
2021-04-15 09:26:16 +0800

    add specify meme function

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = '阿鬼你還是說中文吧!'
+    r = ''
     num_random_meme = random.randint(0,41)
 
     if msg == '梗圖':
