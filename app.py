44849e8eb55a42f5726a7d9374a5d968dbd4ed47
Higgs <jack20425@gmail.com>
2021-04-15 08:03:50 +0800

    add specify meme function

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('osxi9o3k8uGzb6i0qJKtvphiwpgghJu9rVXZoIhTzLnQskoil75p0Qv61qi84UgRN+OaLwYJl6N3ZYhv7Jimndn6n59XxRB1paI92wGcEjXi298uD2x30efq+PZggzpY/trmln94TpI9j5Wxw9/l0wdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('83dc7c0d301e53c5d5216759babb431e')
 
-# num_random_meme = random.randint(0,41)
+num_random_meme = random.randint(0,41)
 
-def pick_memes():
-    num = random.randint(0,41)
+def pick_memes(num):
+    # num = random.randint(0,41)
     collection = ['https://i.imgur.com/CFnfZmD.jpg', 'https://i.imgur.com/tN7r7Xb.jpg', 'https://i.imgur.com/pPka4NU.jpg', 'https://i.imgur.com/MnQ6r96.jpg', 'https://i.imgur.com/PXUBM8r.jpg', 'https://i.imgur.com/c0shKWO.jpg',
     'https://i.imgur.com/n6ysQ1q.jpg', 'https://i.imgur.com/pPOULBM.jpg' ,'https://i.imgur.com/wodXcnU.jpg' ,'https://i.imgur.com/iUMJXub.jpg' ,'https://i.imgur.com/0eTMksx.jpg', 'https://i.imgur.com/unBS2BD.jpg',
     'https://i.imgur.com/67KujPY.jpg', 'https://i.imgur.com/wlgb4E6.jpg', 'https://i.imgur.com/CR1F95O.jpg', 'https://i.imgur.com/HDi18W8.jpg', 'https://i.imgur.com/gbvg8uW.jpg', 'https://i.imgur.com/HndKvCG.jpg',
@@ -57,7 +57,7 @@ def handle_message(event):
     r = '阿鬼你還是說中文吧!'
 
     if msg == '梗圖':
-        meme = pick_memes()
+        meme = pick_memes(num_random_meme)
         image_message = ImageSendMessage(
             original_content_url='https://i.imgur.com/tN7r7Xb.jpg',
             preview_image_url=meme
