63b2c2193867916ba0be6136baf4b66a6e632e05
Higgs <jack20425@gmail.com>
2021-04-15 13:39:41 +0800

    add content data

--- app.py
+++ app.py
@@ -18,17 +18,23 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('osxi9o3k8uGzb6i0qJKtvphiwpgghJu9rVXZoIhTzLnQskoil75p0Qv61qi84UgRN+OaLwYJl6N3ZYhv7Jimndn6n59XxRB1paI92wGcEjXi298uD2x30efq+PZggzpY/trmln94TpI9j5Wxw9/l0wdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('83dc7c0d301e53c5d5216759babb431e')
 
-
-
-def pick_memes(num):
-    collection = ['https://i.imgur.com/CFnfZmD.jpg', 'https://i.imgur.com/tN7r7Xb.jpg', 'https://i.imgur.com/pPka4NU.jpg', 'https://i.imgur.com/MnQ6r96.jpg', 'https://i.imgur.com/PXUBM8r.jpg', 'https://i.imgur.com/c0shKWO.jpg',
+def pick_up_memes(num):
+    memes_library = ['https://i.imgur.com/CFnfZmD.jpg', 'https://i.imgur.com/tN7r7Xb.jpg', 'https://i.imgur.com/pPka4NU.jpg', 'https://i.imgur.com/MnQ6r96.jpg', 'https://i.imgur.com/PXUBM8r.jpg', 'https://i.imgur.com/c0shKWO.jpg',
     'https://i.imgur.com/n6ysQ1q.jpg', 'https://i.imgur.com/pPOULBM.jpg' ,'https://i.imgur.com/wodXcnU.jpg' ,'https://i.imgur.com/iUMJXub.jpg' ,'https://i.imgur.com/0eTMksx.jpg', 'https://i.imgur.com/unBS2BD.jpg',
     'https://i.imgur.com/67KujPY.jpg', 'https://i.imgur.com/wlgb4E6.jpg', 'https://i.imgur.com/CR1F95O.jpg', 'https://i.imgur.com/HDi18W8.jpg', 'https://i.imgur.com/gbvg8uW.jpg', 'https://i.imgur.com/HndKvCG.jpg',
     'https://i.imgur.com/MPnqMVI.jpg', 'https://i.imgur.com/irg2G6L.jpg', 'https://i.imgur.com/x8hHbD2.jpg', 'https://i.imgur.com/I5AW3g2.jpg', 'https://i.imgur.com/0UVfIYL.jpg', 'https://i.imgur.com/VzlUWMC.jpg',
     'https://i.imgur.com/cBj0Bfx.jpg', 'https://i.imgur.com/VaIoKYM.jpg', 'https://i.imgur.com/lbbIFKS.jpg', 'https://i.imgur.com/5Djp6H3.jpg', 'https://i.imgur.com/4kuf6pe.jpg', 'https://i.imgur.com/n9SS2nj.jpg',
     'https://i.imgur.com/YCk5Qus.jpg', 'https://i.imgur.com/sHjTwkz.jpg', 'https://i.imgur.com/X1QXx6I.jpg', 'https://i.imgur.com/1dwRxPX.jpg', 'https://i.imgur.com/2UcI2sG.jpg', 'https://i.imgur.com/kt84g6H.jpg',
     'https://i.imgur.com/rJ2eO3W.jpg', 'https://i.imgur.com/k4IotO1.jpg', 'https://i.imgur.com/42flWYE.jpg', 'https://i.imgur.com/pDDNhjK.jpg', 'https://i.imgur.com/bqDopSu.jpg', 'https://i.imgur.com/DjfVaXW.jpg']
-    return collection[num]
+    return memes_library[num]
+
+
+def reply_message(msg):
+    bullshit_library = {'愈哥': '諭哥沒在上班啦!', '玉哥': '諭哥沒在上班啦!', '諭哥': '諭哥沒在上班啦!', '小業': 'RAV4準備開出來了!', '胞弟': '有房有老婆有小孩,人生勝利組', '意義': '有意義沒逸逸', 
+    '逸逸': '在釣魚啦!', '呆寶': '電腦砸了 遊戲刪了', '偉航': '辣個賺十萬的男人', '胖安': '這很林老闆', '開命': '有400萬的藍人', '阿里': '阿里巴巴', '小馬雲': '阿里 阿里巴巴', '蒼哥': '蒼哥在默默操盤啦', 
+    '倉哥': '蒼哥在默默操盤啦', '吉哥': '吉丸吉丸', '雞哥': '吉丸吉丸', '機哥': '吉丸吉丸', '基哥': '吉丸吉丸', '靠北': '嗯?', '告北': '嗯?', '不是阿': '嘿?', '不是啊': '嘿?', '會癢': '要驗喔', '會癢?': '要驗喔', 
+    '真的來了': '當天再約?', '很快': '有小業快?', '很快餒': '有小業快?', '快喔': '諭哥準備交割?'}
+    return bullshit_library[msg]
 
 
 @app.route("/callback", methods=['POST'])
@@ -63,10 +69,10 @@ def handle_message(event):
             TextSendMessage(text=r))
 
     elif msg == '梗圖啦':
-        meme = pick_memes(num_random_meme)
+        meme_url = pick_up_memes(num_random_meme)
         image_message = ImageSendMessage(
             original_content_url='https://i.imgur.com/tN7r7Xb.jpg',
-            preview_image_url=meme
+            preview_image_url=meme_url
         )
 
         line_bot_api.reply_message(event.reply_token, image_message)
@@ -76,10 +82,10 @@ def handle_message(event):
             num_meme = int(msg.split()[1])
             if isinstance(num_meme, int) == True and num_meme > 0:
                 num_meme -= 1
-                meme = pick_memes(num_meme)
+                meme_url = pick_up_memes(num_meme)
                 image_message = ImageSendMessage(
                     original_content_url='https://i.imgur.com/tN7r7Xb.jpg',
-                    preview_image_url=meme
+                    preview_image_url=meme_url
                 )
 
                 line_bot_api.reply_message(event.reply_token, image_message)
@@ -213,56 +219,10 @@ def handle_message(event):
         event.reply_token,
         carousel_template_message)
 
-    if msg == '諭哥':
-        r = '諭哥沒在上班啦!'
-    elif msg == '小業':
-        r = 'RAV4準備開出來了!'
-    elif msg == '胞弟':
-        r = '有房有老婆有小孩,人生勝利組'
-    elif '胞弟' in msg:
-        r = '又再說胞弟壞話?'
-    elif msg == '意義':
-        r = '有意義沒逸逸'
-    elif msg == '逸逸':
-        r = '在釣魚啦'
-    elif msg == '呆寶':
-        r = '遊戲刪了'
-    elif msg == '偉航':
-        r = '辣個賺十萬的男人'
-    elif msg == '林老闆':
-        r = '林老闆帶大家飛'
-    elif msg == '開命':
-        r = '有400萬的藍人'
-    elif '小馬雲' in msg:
-        r = '阿里 阿里巴巴'
-    elif msg == '蒼哥':
-        r = '蒼哥在默默操盤'
-    elif msg == '...':
-        r = '...'
-    elif msg == '靠北':
-        r = '恩?'
-    elif '不是阿' in msg or '不是啊' in msg or '不是' in msg:
-        r = '嘿?'
-    elif '癢' in msg:
-        r = '要驗喔'
-    elif '高雄' in msg:
-        r = '真的來了!'                        
-    elif '快' in msg:
-        r = '有小業快?'
+    if msg:
+        r = reply_message(msg)
     # elif '' in msg:
     #     r = ''
-    # elif '' in msg:
-    #     r = ''
-    # elif '' in msg:
-    #     r = ''
-    # elif '' in msg:
-    #     r = ''
-    # elif '' in msg:
-    #     r = ''
-    # elif '' in msg:
-    #     r = ''
-    # elif '諭哥' in msg:
-    #     r = '又有諭哥的局了?'
 
     line_bot_api.reply_message(
         event.reply_token,
